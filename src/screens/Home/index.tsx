e0e63438b842694b8bba9a8c0ed1bf9b94f7b14b
Lorena Mendes Barbosa <75842072+LorenaMendesB@users.noreply.github.com>
2022-03-30 16:31:18 -0300

    2

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import {styles} from './styles';
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Bem-vindo!</Text>
+    </View>
+  );
+}
